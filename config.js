eaed1154fb7f3f0fbbc110eb1b7e2a2a67cef14e
Nguyenhuumanh <78522720+manhkj@users.noreply.github.com>
2021-10-20 22:56:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi tới con lợn đáng iu",
+    introTitle: 'Đặng Thị Ngọc Mai',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnIntro: '^^Hmm^^',
+    title: 'Hôm nay ngày 20/10 anh có đôi lời muốn gửi em',
+    desc: 'A yêu em , chúc em ngày 20/10 vui vẻ , làm vợ a nhé baby ? ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    question: 'Nhân ngày 20/10 em đồng ý làm vợ a nhé  <3',
+    btnReply: 'Gửi cho Minh Thế <3',
+    reply: 'Em đồng ý làm vợ của a ',
+    mess: 'Anh biết mà. Yêu em nhiều 😘😘',
+    messDesc: 'Chúc em ngày 20/10 vui vẻ . Cho a gửi lời chúc tới mẹ của em lun nha',
+    btnAccept: 'Ok anh yêu <3',
+    messLink: 'https://m.me/minhthe.bui.161' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
