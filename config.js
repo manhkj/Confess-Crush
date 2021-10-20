b176bf54480f5549bbd3ca99b38c6bddb91ea1b9
Nguyenhuumanh <78522720+manhkj@users.noreply.github.com>
2021-10-20 23:31:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: " Gửi con lợn của a ",
-    introTitle: 'Đặng Thị Ngọc Mai',
+    introTitle: 'Đặng Ngọc Mai',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
